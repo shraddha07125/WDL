eff8b694189519587ea2a9f85fe04e2eac1de57b
Shraddhapawar <shraddhapawar7005@gmail.com>
2026-04-20 12:28:41 +0530

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,5 @@
+<form action="insert.php" method="POST">
+    <input type="text" name="name" placeholder="Product Name" required>
+    <input type="number" name="price" placeholder="Price" required>
+    <button>Add Product</button>
+</form>
